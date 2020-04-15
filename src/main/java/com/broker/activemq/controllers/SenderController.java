98aa62395cc3d9df8aa15190a149c552ae330dc3
JoseLuisSR <jose.luis.sastoque.rey@gmail.com>
2020-04-14 20:20:58 -0500

    Build RESTful API to put message on queue and set up JMeter to consume API.

--- src/main/java/com/broker/activemq/controllers/SenderController.java
+++ src/main/java/com/broker/activemq/controllers/SenderController.java
@@ -4,10 +4,7 @@ import com.broker.activemq.entities.Message;
 import com.broker.activemq.services.SenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "${sender.context.path}")
@@ -15,6 +12,11 @@ public class SenderController {
 
     @Autowired
     public SenderService senderService;
+
+    @PostMapping(path = "${sender.sendMessage}")
+    public void send(@PathVariable("message") String message){
+        senderService.send(new Message(message));
+    }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
     public void send(@RequestBody Message message){
